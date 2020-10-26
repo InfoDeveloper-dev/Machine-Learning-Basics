acba4fc29ecffb50db671ce691e5523fa0e94232
satinder <71826291+InfoDeveloper-dev@users.noreply.github.com>
2020-10-26 18:19:51 +0530

    Updated TFIDF

--- text_features/text_features_creation.py
+++ text_features/text_features_creation.py
@@ -7,6 +7,7 @@ Program is to implement two feature extraction techniques of text
 
 import pandas as pd
 import re
+import math 
 
 dict_text_featutes = {
     'Models': ['BOW', 'TFIDF'],
@@ -97,9 +98,87 @@ text_to_features = feat_from_vocab(
                    text_for_features_lo,
                    text_to_feat_vocab   
                    )
+print("Result using BAG OF WORDS APPROACH")
+print("-"*50)
 print(
-     "Text to features for the sentence: {} is: {}".format(
+     "Text is: {}\nand features are:\n {}".format(
       text_for_features, 
       text_to_features   
      )
 )     
+print("-"*50)
+"""
+Implementing TFIDF 
+
+TF: Term Frequency i.e num of times a word appers in document/
+                       total number of words in the document
+
+IDF: Inverse Document Frequency i.e 
+                    log(number of document/documents where words 
+                    appears)
+
+"""
+doc1 = "My name is developer"
+doc2 = "I love python"
+doc3 = "python is my passion"
+
+
+def all_cases_to_lower(string):
+
+    string = string.lower()
+    return string
+
+
+def term_frequency(document, word):
+    
+    document = all_cases_to_lower(document)
+    word = all_cases_to_lower(word)
+
+    doc_list = document.split()
+    len_of_doc = len(doc_list)
+    
+    if word in doc_list:
+        document_count = doc_list.count(word)
+    else:
+        print("select word from the document only")
+    term_freq = (document_count/len_of_doc)
+
+    return term_freq
+
+
+def inverse_frequency_document(total_document, word):
+
+    len_documents = len(total_document)
+    count = 0
+    word = all_cases_to_lower(word)
+    for i in range(len_documents):
+        if word in total_document[i]:
+            count += 1
+    
+    idf_result = math.log(len_documents/count) 
+
+    return idf_result
+
+# calculating the term frequency of each word in document 1
+doc1_in_list = doc1.split()
+print()
+print("TF Calculation for Document 1")
+print("Word".ljust(20), "Term Frequency")
+print()
+for each_word in doc1_in_list:
+    term_fre = term_frequency(doc1, each_word)
+    print(each_word.ljust(20), term_fre)
+
+documents_list = [doc1, doc2, doc3]
+document_list_lo = [i.lower() for i in documents_list]
+print()
+print("IDF Calculation for word python")
+print("Word".ljust(20), "Inverse Document Frequency")
+python_idf = inverse_frequency_document(
+             document_list_lo,
+             "Python"
+    ) 
+print("Python".ljust(20), python_idf)
+
+
+
